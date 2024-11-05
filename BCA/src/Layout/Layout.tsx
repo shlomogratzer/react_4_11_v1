e9f5d8227434937e1ab8e9b2e0108c40d8f7d92d
shlomo <shlomogratzer1@gmail.com>
2024-11-05 11:02:08 +0200

    first commit

---
+++ BCA/src/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import Header from "./header/Header"
+import Main from "./main/Main"
+import Footer from "./footer/Footer"
+
+const Layout = () => {
+  return (
+   <>
+   <Header/>
+   <Main/>
+   <Footer/>
+   </>
+  )
+}
+
+export default Layout
